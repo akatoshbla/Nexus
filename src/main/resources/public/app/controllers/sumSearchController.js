753997c46726003492f21bc53de0002b5bda62ea
gho5tone <itsphilli@gmail.com>
2016-02-08 19:16:52 -0800

    search proto
    attempting to config search with angular

--- src/main/resources/public/app/controllers/sumSearchController.js
+++ src/main/resources/public/app/controllers/sumSearchController.js
@@ -3,23 +3,23 @@
  */
 (function(){
     var sumSearchController = function ($scope, $http){
-        var summoner = "https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/gho5tone?api_key=6de076c3-3dc7-4efc-9566-a5dfae3003b3";
+        console.log($scope);
+        console.log($http);
         var mastery = "https://na.api.pvp.net/api/lol/na/v1.4/summoner/gho5tone";
+        console.log($scope.sumName);
+           $scope.search = function(){
+            console.log(sumName);
+            var summoner = "https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/"+ $scope.sumName+ "?api_key=6de076c3-3dc7-4efc-9566-a5dfae3003b3";
 
-        if($scope.search === undefined){
-            $scope.search = "gho5tone";
-            retrieve();
-        }
-        function retrieve(){
-        $http.get(summoner).success(function(response){
-            $scope.sumId = response.id;
-            $scope.sumName = response.name;
-            $scope.profileIcon = response.profileIconId;
-            $scope.sumLvl = response.summonerLevel;
-            $scope.revisDate = response.revisionDate;
+            $http.get(summoner).success(function(response){
+            //$scope.sumId = response.id;
+            //$scope.sumName = response.name;
+            //$scope.profileIcon = response.profileIconId;
+            //$scope.sumLvl = response.summonerLevel;
+            //$scope.revisDate = response.revisionDate;
 
             console.log("response:", response);
-            $scope.info = response[$scope.search];
+            $scope.info = response[$scope.sumName];
         });
         }
     };
