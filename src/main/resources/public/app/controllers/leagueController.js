6c608de7537691bd14d488d57945ac010f69639e
BenDluzak <ben.dluzak@gmail.com>
2016-02-08 19:32:00 -0800

    Moved search function to league controller

--- src/main/resources/public/app/controllers/leagueController.js
+++ src/main/resources/public/app/controllers/leagueController.js
@@ -25,12 +25,28 @@
 
             })
             $scope.combinedObject.push($scope.champData);
-
-
-
-            console.log($scope.combinedObject);
+       
 
         })
+           
+        var mastery = "https://na.api.pvp.net/api/lol/na/v1.4/summoner/gho5tone";
+       
+           $scope.search = function(){
+            console.log($scope.sumName);
+            var summoner = "https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/"+ $scope.sumName+ "?api_key=6de076c3-3dc7-4efc-9566-a5dfae3003b3";
+
+            $http.get(summoner).success(function(response){
+            //$scope.sumId = response.id;
+            //$scope.sumName = response.name;
+            //$scope.profileIcon = response.profileIconId;
+            //$scope.sumLvl = response.summonerLevel;
+            //$scope.revisDate = response.revisionDate;
+
+            console.log("response:", response);
+            $scope.info = response[$scope.sumName];
+        });
+        }
+       
 
     };
 
--- src/main/resources/public/app/controllers/sumSearchController.js
+++ src/main/resources/public/app/controllers/sumSearchController.js
@@ -3,25 +3,7 @@
  */
 (function(){
     var sumSearchController = function ($scope, $http){
-        console.log($scope);
-        console.log($http);
-        var mastery = "https://na.api.pvp.net/api/lol/na/v1.4/summoner/gho5tone";
-        console.log($scope.sumName);
-           $scope.search = function(){
-            console.log(sumName);
-            var summoner = "https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/"+ $scope.sumName+ "?api_key=6de076c3-3dc7-4efc-9566-a5dfae3003b3";
-
-            $http.get(summoner).success(function(response){
-            //$scope.sumId = response.id;
-            //$scope.sumName = response.name;
-            //$scope.profileIcon = response.profileIconId;
-            //$scope.sumLvl = response.summonerLevel;
-            //$scope.revisDate = response.revisionDate;
-
-            console.log("response:", response);
-            $scope.info = response[$scope.sumName];
-        });
-        }
+    
     };
 
 
