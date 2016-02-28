4152642da6a2d038cfd649b68b1db7af71b66fc4
gho5tone <itsphilli@gmail.com>
2016-02-28 11:50:38 -0800

    updateFromMaster

--- src/main/resources/public/app/controllers/worldOfWarcraftController.js
+++ src/main/resources/public/app/controllers/worldOfWarcraftController.js
@@ -1,7 +1,18 @@
 (function () {
+    var key = 't5bqm7xpt3kzu4u3jxrasec9pje5nvrp';
+    var secret = '86dFFh3zDwGkejhkeDmUhjB3HKj2aeeQ';
 
     var worldOfWarcraftController = function ($scope, $http) {
-    
+         $scope.characterLookup = function (realm, characterName) {
+           var  host = "https://us.api.battle.net/wow/character/";
+            var request = host + realm.replace(" " , "-") + '/' + characterName + "?fields=guild,items,pvp,appearance,progression&locale=en_US&&apikey=t5bqm7xpt3kzu4u3jxrasec9pje5nvrp";
+             $http.get(request).success( function(response){
+                 console.log(response);
+                 $scope.characterInfo = response;
+             })
+            
+
+        }
     };
 
     worldOfWarcraftController.$inject = ['$scope' , '$http'];
