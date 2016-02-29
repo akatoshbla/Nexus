5aa897f3882b57647632dc050b3996de7b55bd86
Alex Hall <alexhall1093@gmail.com>
2016-02-28 22:53:30 -0800

    Removed commented out code

--- src/main/java/com/nexus/MessageService.java
+++ src/main/java/com/nexus/MessageService.java
@@ -8,7 +8,6 @@ import java.util.List;
 import org.json.simple.JSONObject;
 import org.json.simple.JSONArray;
 import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 
 /**
  * This class has the methods which support MessageController.
@@ -56,35 +55,5 @@ public class MessageService {
 			e.printStackTrace();
 			return null;
 		}
-	//	return null; //placeholder
-	}
-	/*
-	public JsonObject messageResult(String body) throws ParseException {
-		JSONObject json = (JSONObject) new JSONParser().parse(body);
-		boolean val =  sendMessage((String)json.get("message"), (String)json.get("fromUser"),
-				(String)json.get("toUser"));
-		JsonObject result = new JsonObject();
-		result.addProperty("result",val);
-		return result;
-	}*/
-	public static void main(String[] args) throws ParseException {
-		String obj = "{\"time\":\"test\",\"fromUser\":\"test5\","
-				+ "\"toUser\":\"demotest3\",\"message\":\"hello\"}";
-		MessageService ms = new MessageService();
-	//	System.out.println(ms.sendMessage(obj)); 
-		ms.getMessages(obj);
-		
-		
-		/*
-		String obj = "{\"messages\":[{\"time\":\"test\",\"from\":\"test1\","
-				+ "\"to\":\"test2\",\"message\":\"hello\"},{\"time\":\"test\","
-				+ "\"from\":\"test1\",\"to\":\"test2\",\"messge\":\"hello\"}]}";
-		JSONObject json = (JSONObject) new JSONParser().parse(obj);
-		System.out.println(json.get("messages"));
-		JSONArray arr = (JSONArray)json.get("messages");
-		System.out.println(arr);
-		for(Object o: arr) {
-			System.out.println( ((JSONObject)o).get("from") );
-		}*/
 	}
 }
